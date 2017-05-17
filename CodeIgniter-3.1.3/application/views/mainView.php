4e0ff09c9f5e206a416c14427ab3d6133ca086a4
Paulo Silva <a22297@ualg.pt>
2017-05-17 12:35:54 +0100

    US#12 RF5 task#30 Adicionado caixa para mensagem e email

--- CodeIgniter-3.1.3/application/views/mainView.php
+++ CodeIgniter-3.1.3/application/views/mainView.php
@@ -54,15 +54,46 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 										class="w3-btn w3-red w3-round w3-display-topright" style="font-size:16px; text-shadow:2px 1px 0 #444;">&times;</span>
 									 <h3 class="w3-center" style="text-shadow:2px 1px 0 #444;">Frequently Asked Questions</h3>
 								</header>
-								<div id="id02" class="w3-container w3-text-black">
-									<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
-									<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
-									<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
-									<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
-									<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+								<div id="id02" class="w3-container w3-text-black ">
+									<div class="w3-container">
+										<br>
+										<div class="w3-container w3-card-4">
+										
+										<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+									    <p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+										<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+									    <p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+										<p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+									    <p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p><p>Some text..</p>
+									</div>
+									</div>
+								<!-- FORM -->
+								<!-- Alterar a /action_page.php -->
+									<form action="/action_page.php">
+										<div class="w3-container w3-card-4 w3-light-grey w3-text-blue w3-margin">
+										<h3 class="w3-center">Contact Us</h3>
+										 
+										<div class="w3-row w3-section">
+										  <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-envelope-o"></i></div>
+											<div class="w3-rest">
+											  <input class="w3-input w3-border" name="email" type="text" placeholder="Email">
+											</div>
+										</div>
+
+										<div class="w3-row w3-section">
+										  <div class="w3-col" style="width:50px"><i class="w3-xxlarge fa fa-pencil"></i></div>
+											<div class="w3-rest">
+											  <input class="w3-input w3-border" name="message" type="text" placeholder="Message">
+											</div>
+										</div>
+
+										<button class="w3-button w3-block w3-section w3-blue w3-ripple w3-padding">Send</button>
+										</div>
+									</form>
+								<p>
 								</div>
 								<footer class="w3-container w3-blue">
-									<p>Modal Footer</p>
+								
 								</footer>
 							</div>
 						</div>
